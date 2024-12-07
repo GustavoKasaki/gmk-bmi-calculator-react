1a1136e07b4c8964ec9a7b602d0f7b93e843e3c0
GustavoKasaki <gmkasaki@gmail.com>
2024-12-07 08:49:55 -0300

    chore: updated components

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -7,40 +7,43 @@ const Result = () => {
                 <h3>Your BMI is:</h3>
                 <span>20 kg/m²</span>
             </div>
-            <ul>
-                <li>
-                    <p>Severe Thinness</p>
-                    <p>{'<16'}</p>
-                </li>
-                <li>
-                    <p>Moderate Thinness</p>
-                    <p>16 ~ 17</p>
-                </li>
-                <li>
-                    <p>Mild Thinness</p>
-                    <p>17 ~ 18.5</p>
-                </li>
-                <li>
-                    <p>Normal</p>
-                    <p>16 ~ 17</p>
-                </li>
-                <li>
-                    <p>Overweight</p>
-                    <p>16 ~ 17</p>
-                </li>
-                <li>
-                    <p>Obese Class I</p>
-                    <p>16 ~ 17</p>
-                </li>
-                <li>
-                    <p>Obese Class II</p>
-                    <p>16 ~ 17</p>
-                </li>
-                <li>
-                    <p>Obese Class III</p>
-                    <p>{'<16'}</p>
-                </li>
-            </ul>
+            <div className="chart">
+                <h3 className='chart__title'>BMI values for adults:</h3>
+                <ul className='chart__table'>
+                    <li className='chart__table__item'>
+                        <p>Severe Thinness</p>
+                        <p>{'<16'}</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Moderate Thinness</p>
+                        <p>16 ~ 17</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Mild Thinness</p>
+                        <p>17 ~ 18,5</p>
+                    </li>
+                    <li className='chart__table__item chart__table__item--isActive'>
+                        <p>Normal</p>
+                        <p>18,5 ~ 25</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Overweight</p>
+                        <p>25 ~ 30</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Obese Class I</p>
+                        <p>30 ~ 35</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Obese Class II</p>
+                        <p>35 ~ 40</p>
+                    </li>
+                    <li className='chart__table__item'>
+                        <p>Obese Class III</p>
+                        <p>{'>40'}</p>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
